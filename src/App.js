ccb8d0d4b1b5738307046ea2897f576f0717ef4a
Santhosh B. Appan <12586088+sbappan@users.noreply.github.com>
2022-01-29 18:49:58 +0530

    add API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    (async function () {
+      const { text } = await( await fetch(`/api/message`)).json();
+      console.log('text: ', text);
+      setData(text);
+    })();
+  });
+
+  return <div>{data}</div>;
 }
 
 export default App;
